5db73fded36bd248c25b1d25442a6194a0c5cbb1
mirian-lima <mirianevangelistalima@gmail.com>
2026-03-19 11:15:44 -0300

    refactor: adiciona ChromeOptions no DriverFactory para desativar popups e melhorar estabilidade dos testes

--- src/main/java/core/DriverFactory.java
+++ src/main/java/core/DriverFactory.java
@@ -3,18 +3,33 @@ package core;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DriverFactory {
 
     private static WebDriver driver;
 
-    private DriverFactory() {
-    }
+    private DriverFactory() {}
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            driver = new ChromeDriver();
-            // hard code pois está dando bug com maximize por meu SO ser linux
+            ChromeOptions options = new ChromeOptions();
+            Map<String, Object> prefs = new HashMap<>();
+            prefs.put("credentials_enable_service", false);
+            prefs.put("profile.password_manager_enabled", false);
+            prefs.put("profile.password_manager_leak_detection", false);
+
+
+            options.setExperimentalOption("prefs", prefs);
+            options.addArguments("--disable-notifications");
+            options.addArguments("--remote-allow-origins=*");
+
+            driver = new ChromeDriver(options);
+
+            // Workaround para problema de maximize no Linux
             driver.manage().window().setSize(new Dimension(1920, 1080));
         }
         return driver;
@@ -26,6 +41,4 @@ public class DriverFactory {
             driver = null;
         }
     }
-
-
 }
